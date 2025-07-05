8aba36cfa1ed768b1dbecf49279680b4752cb7af
Urbanlegend123 <greenchamps123@gmail.com>
2025-07-05 01:29:39 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+import { log } from "console"
+
+const accountId = 144544
+let accountEmail = "greenchamps123"
+var accountPassword = "12345"
+// accountCity = "jaipur"
+let accountState 
+
+// accountId = 2 
+
+console.log(accountId);
+console.log(accountState)
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+import { log } from "console";
+
+ // treat all js code as newer version
+
+// data types 
+// number => 2 to power 53 
+// bigint 
+// string ""
+// null => standalone value 
+// undefined =>
+// symbol 
+
+// object 
+
+// read about alert in node and brower, difference between null and undefined and data types  
+
+console.log(typeof "shanmukh");
+console.log(typeof null); // output is object 
+console.log(typeof undefined); // output is undefined 
+
