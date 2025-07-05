9bc6fa44d7db02edcdbb41f55d7077f02ac2a486
Urbanlegend123 <greenchamps123@gmail.com>
2025-07-05 02:01:56 +0000

    conversion operations

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,62 @@
+//  // let score = 33
+//  console.log(typeof score);
+//  console.log(typeof (score));
+ 
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);  output will be number 
+
+
+//  let score = "33"
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+
+
+//  let score = null
+//  console.log(score); // output is null 
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber); // output is 0
+ 
+ 
+// let score = undefined;
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log( valueInNumber) output is NaN
+
+//  let score = true
+//  console.log(score); // output is true
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+
+
+//   let score = "hello"
+//  console.log(score);
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber);
+//  console.log(valueInNumber); // output is nan
+
+//   let score = "55"
+//  console.log(score);
+//  console.log(typeof score); // type is string 
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber); // type becomes number 
+//  console.log(valueInNumber); // output is number ie 55 
+
+
+//   let score = "55abc"
+//  console.log(score);
+//  console.log(typeof score); // type is string 
+//  let valueInNumber = Number(score);
+//  console.log(typeof valueInNumber); // type becomes number 
+//  console.log(valueInNumber); // output becomes NaN
+//  let isLoggedIn = 0
+//  console.log(typeof isLoggedIn);
+//  let booleanLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanLoggedIn); 
+// 1 or any numeber => true 0 => false 
+ // "" => false "hello" => true
+ 
+
+
+ 
